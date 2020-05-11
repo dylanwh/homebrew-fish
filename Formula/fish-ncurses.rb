5c4fcfab261199497479bf6496d6ec501701d783
Dylan Hardison <dylan@hardison.net>
2020-05-10 22:36:03 -0400

    initial version

---
+++ Formula/fish-ncurses.rb
@@ -0,0 +1,43 @@
+class FishNcurses < Formula
+  desc "User-friendly command-line shell for UNIX-like operating systems (linked with brew ncurses)"
+  homepage "https://fishshell.com"
+  url "https://github.com/fish-shell/fish-shell/releases/download/3.1.2/fish-3.1.2.tar.gz"
+  sha256 "d5b927203b5ca95da16f514969e2a91a537b2f75bec9b21a584c4cd1c7aa74ed"
+
+  head do
+    url "https://github.com/fish-shell/fish-shell.git", :shallow => false
+
+    depends_on "sphinx-doc" => :build
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pcre2"
+  depends_on "ncurses"
+
+
+  def install
+    # In Homebrew's 'superenv' sed's path will be incompatible, so
+    # the correct path is passed into configure here.
+    ncurses_ver = Dir.children("#{HOMEBREW_PREFIX}Cellar/ncurses").sort.last
+    args = %W[
+      -Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d
+      -Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
+      -Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d
+      -DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}/Cellar/ncurses/#{ncurses_ver}
+      -DSED=/usr/bin/sed
+    ]
+    system "sed", '/CODESIGN_ON_MAC(${target})/ d', 'CMakeLists.txt'
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+
+  def post_install
+    (pkgshare/"vendor_functions.d").mkpath
+    (pkgshare/"vendor_completions.d").mkpath
+    (pkgshare/"vendor_conf.d").mkpath
+  end
+
+  test do
+    system "#{bin}/fish", "-c", "echo"
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Dylan's Fish That Does Not Use Old Ncurses
+
+## How do I install these formulae?
+`brew install dylanwh/fish/fish-ncurses`
+
+Or `brew tap dylanwh/fish` and then `brew install fish-ncurses`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/dylanwh/homebrew-fish/master/Formula/fish-ncurses.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
